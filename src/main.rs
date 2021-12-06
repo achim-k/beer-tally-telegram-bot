382429a0068d14a51987191c5ceba1d2ac4e05bf
Hans-Joachim Krauch <achim.krauch@gmail.com>
2021-12-06 17:37:32 +0000

    Add beer tally logic with only register/unregister for now.

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,63 @@ use teloxide::{prelude::*, utils::command::BotCommand};
 
 use std::error::Error;
 
+mod storage;
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+use storage::{BeerTally, HashMapBeerTally, RegisterPlayerResult};
+
+lazy_static! {
+    static ref STORAGE: Mutex<Box<dyn BeerTally + Send + Sync>> =
+        Mutex::new(Box::new(HashMapBeerTally::new()));
+}
+
 #[derive(BotCommand)]
 #[command(rename = "lowercase", description = "These commands are supported:")]
 enum Command {
     #[command(description = "display this text.")]
     Help,
-    #[command(description = "select someone who has to pay the beers.")]
-    PayTheBeers(String),
+    #[command(description = "register as a player.")]
+    Register(String),
+    #[command(description = "unregister as a player.")]
+    Unregister,
 }
 
 async fn answer(
     cx: UpdateWithCx<AutoSend<Bot>, Message>,
     command: Command,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let chat_id = cx.update.chat_id();
+    let user_id = cx.update.from().unwrap().id;
+
     match command {
         Command::Help => cx.answer(Command::descriptions()).await?,
-        Command::PayTheBeers(username) => {
-            cx.answer(format!("{} pays the beers 🍻", username)).await?
+        Command::Register(username) => {
+            let return_string = match STORAGE
+                .lock()
+                .unwrap()
+                .register_player(chat_id, user_id, &username)
+            {
+                RegisterPlayerResult::InvalidUsername => {
+                    String::from("Invalid username. Only alphanumeric characters are allowed.")
+                }
+                RegisterPlayerResult::Registered => {
+                    format!("Successfully registered as '{}'.", &username)
+                }
+                RegisterPlayerResult::AlreadyRegistered(existing_name) => {
+                    format!("You are already registered as '{}'. Use command /change_name to change your username.", existing_name)
+                }
+                RegisterPlayerResult::UsernameTaken => {
+                    format!("Username '{}' is already taken.", &username)
+                }
+            };
+            cx.answer(return_string).await?
+        }
+        Command::Unregister => {
+            let return_string = match STORAGE.lock().unwrap().unregister_player(chat_id, user_id) {
+                Ok(_) => String::from("Successfully unregistered."),
+                Err(_) => String::from("You were not registered."),
+            };
+            cx.answer(return_string).await?
         }
     };
 
---
+++ src/storage.rs
@@ -0,0 +1,75 @@
+pub enum RegisterPlayerResult {
+    InvalidUsername,
+    AlreadyRegistered(String),
+    UsernameTaken,
+    Registered,
+}
+
+pub trait BeerTally {
+    fn register_player(
+        &mut self,
+        chat_id: i64,
+        user_id: i64,
+        username: &str,
+    ) -> RegisterPlayerResult;
+    fn unregister_player(&mut self, chat_id: i64, user_id: i64) -> Result<(), ()>;
+}
+
+use std::collections::HashMap;
+
+pub struct HashMapBeerTally {
+    /// Player names for a given chat id and user id.
+    /// In other words, the same user can participate in multiple chats.
+    players: HashMap<i64, HashMap<i64, String>>,
+}
+
+impl HashMapBeerTally {
+    pub fn new() -> HashMapBeerTally {
+        HashMapBeerTally {
+            players: HashMap::new(),
+        }
+    }
+}
+
+impl BeerTally for HashMapBeerTally {
+    fn register_player(
+        &mut self,
+        chat_id: i64,
+        user_id: i64,
+        username: &str,
+    ) -> RegisterPlayerResult {
+        if username.is_empty() || !username.chars().all(char::is_alphanumeric) {
+            return RegisterPlayerResult::InvalidUsername;
+        }
+
+        match self.players.get_mut(&chat_id) {
+            None => {
+                let mut usernames = HashMap::new();
+                usernames.insert(user_id, username.to_string());
+                self.players.insert(chat_id, usernames);
+                RegisterPlayerResult::Registered
+            }
+            Some(usernames) => {
+                if let Some(registered_username) = usernames.get(&user_id) {
+                    return RegisterPlayerResult::AlreadyRegistered(
+                        registered_username.to_string(),
+                    );
+                } else if usernames.values().any(|x| x == username) {
+                    return RegisterPlayerResult::UsernameTaken;
+                }
+                usernames.insert(user_id, username.to_string());
+                RegisterPlayerResult::Registered
+            }
+        }
+    }
+
+    fn unregister_player(&mut self, chat_id: i64, user_id: i64) -> Result<(), ()> {
+        match self.players.get_mut(&chat_id) {
+            None => Err(()),
+            Some(usernames) => match usernames.remove(&user_id) {
+                Some(_) => Ok(()),
+                None => Err(()),
+            },
+        }
+    }
+}
